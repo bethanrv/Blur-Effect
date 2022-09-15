caefd5e834e3ceab7a2d70da0d168668737c653d
bethanrv <brv@Brians-MBP.attlocal.net>
2022-09-14 22:35:04 -0400

    starting firefox plugin

---
+++ firefox/icons/blurify.js
@@ -0,0 +1,11 @@
+/*
+ * Blurify js
+ * Allow user to select an html element to blur it
+ * + Control blur intestity
+ * + Allow same class blur
+ *
+ * v 1.0
+*/
+
+// testing
+document.body.style.border = "5px solid red";
